21f96072fc25ffd2807ceca522f56f4b04129545
Yogesh kumar <yogeshkumar12112003@gmail.com>
2024-08-25 15:40:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-xl fon-bold text-center'>
+      
+  
+      
+    </div>
+  )
+}
+
+export default App
